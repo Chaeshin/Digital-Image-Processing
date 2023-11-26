e9005742e2f61210ef92738f2a20b9b8ab6492a0
fronald143 <120772118+fronald143@users.noreply.github.com>
2023-11-27 04:16:19 +0800

    add subtract

--- ImageProcessing/Form1.Designer.cs
+++ ImageProcessing/Form1.Designer.cs
@@ -38,23 +38,37 @@
             this.colorInversionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.histogramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sepiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subtractToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnLoadImageBack = new System.Windows.Forms.Button();
+            this.btnLoadImageFront = new System.Windows.Forms.Button();
+            this.webToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem,
-            this.processToolStripMenuItem});
+            this.processToolStripMenuItem,
+            this.webToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(756, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1274, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -63,21 +77,22 @@
             this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem1,
             this.saveToolStripMenuItem});
+            this.openToolStripMenuItem.Font = new System.Drawing.Font("Lucida Fax", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
             this.openToolStripMenuItem.Text = "File";
             // 
             // openToolStripMenuItem1
             // 
             this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
-            this.openToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(109, 22);
             this.openToolStripMenuItem1.Text = "Open";
             this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
@@ -88,9 +103,11 @@
             this.greyScaleToolStripMenuItem,
             this.colorInversionToolStripMenuItem,
             this.histogramToolStripMenuItem,
-            this.sepiaToolStripMenuItem});
+            this.sepiaToolStripMenuItem,
+            this.subtractToolStripMenuItem});
+            this.processToolStripMenuItem.Font = new System.Drawing.Font("Lucida Fax", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.processToolStripMenuItem.Name = "processToolStripMenuItem";
-            this.processToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
             this.processToolStripMenuItem.Text = "Process";
             // 
             // copyToolStripMenuItem
@@ -128,20 +145,29 @@
             this.sepiaToolStripMenuItem.Text = "Sepia";
             this.sepiaToolStripMenuItem.Click += new System.EventHandler(this.sepiaToolStripMenuItem_Click);
             // 
+            // subtractToolStripMenuItem
+            // 
+            this.subtractToolStripMenuItem.Name = "subtractToolStripMenuItem";
+            this.subtractToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.subtractToolStripMenuItem.Text = "Subtract";
+            this.subtractToolStripMenuItem.Click += new System.EventHandler(this.subtractToolStripMenuItem_Click);
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(45, 43);
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(39, 82);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(315, 227);
+            this.pictureBox1.Size = new System.Drawing.Size(350, 350);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(397, 44);
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(426, 82);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(314, 226);
+            this.pictureBox2.Size = new System.Drawing.Size(350, 350);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
@@ -155,21 +181,125 @@
             // 
             this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 38);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(516, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 38);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Processed";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Enabled = false;
+            this.pictureBox3.Location = new System.Drawing.Point(817, 82);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Enabled = false;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(923, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 38);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Processed";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Enabled = false;
+            this.btnSubtract.Location = new System.Drawing.Point(930, 438);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(137, 45);
+            this.btnSubtract.TabIndex = 8;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnLoadImageBack
+            // 
+            this.btnLoadImageBack.Enabled = false;
+            this.btnLoadImageBack.Location = new System.Drawing.Point(115, 442);
+            this.btnLoadImageBack.Name = "btnLoadImageBack";
+            this.btnLoadImageBack.Size = new System.Drawing.Size(164, 40);
+            this.btnLoadImageBack.TabIndex = 9;
+            this.btnLoadImageBack.Text = "Load Background";
+            this.btnLoadImageBack.UseVisualStyleBackColor = true;
+            this.btnLoadImageBack.Click += new System.EventHandler(this.btnLoadImageBack_Click);
+            // 
+            // btnLoadImageFront
+            // 
+            this.btnLoadImageFront.Enabled = false;
+            this.btnLoadImageFront.Location = new System.Drawing.Point(523, 438);
+            this.btnLoadImageFront.Name = "btnLoadImageFront";
+            this.btnLoadImageFront.Size = new System.Drawing.Size(182, 54);
+            this.btnLoadImageFront.TabIndex = 10;
+            this.btnLoadImageFront.Text = "Load Front";
+            this.btnLoadImageFront.UseVisualStyleBackColor = true;
+            this.btnLoadImageFront.Click += new System.EventHandler(this.btnLoadImageFront_Click);
+            // 
+            // webToolStripMenuItem
+            // 
+            this.webToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem2});
+            this.webToolStripMenuItem.Name = "webToolStripMenuItem";
+            this.webToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.webToolStripMenuItem.Text = "Web";
+            // 
+            // openToolStripMenuItem2
+            // 
+            this.openToolStripMenuItem2.Name = "openToolStripMenuItem2";
+            this.openToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem2.Text = "Open";
+            this.openToolStripMenuItem2.Click += new System.EventHandler(this.openToolStripMenuItem2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(756, 450);
+            this.ClientSize = new System.Drawing.Size(1274, 507);
+            this.Controls.Add(this.btnLoadImageFront);
+            this.Controls.Add(this.btnLoadImageBack);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Image Processing";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -191,6 +321,17 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem subtractToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnLoadImageBack;
+        private System.Windows.Forms.Button btnLoadImageFront;
+        private System.Windows.Forms.ToolStripMenuItem webToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem2;
     }
 }
 
--- ImageProcessing/Form1.cs
+++ ImageProcessing/Form1.cs
@@ -1,18 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+
 
 namespace ImageProcessing
 {
     public partial class Form1 : Form
     {
-        Bitmap loadImage, processedImage;
+        Bitmap loadImage, loadImage2, processedImage;
+
         public Form1()
         {
             InitializeComponent();
@@ -29,8 +28,8 @@ namespace ImageProcessing
             processedImage = new Bitmap(loadImage.Width, loadImage.Height);
             Color pixel;
 
-            for(int x = 0; x<loadImage.Width; x++)
-                for(int y = 0; y<loadImage.Height; y++)
+            for (int x = 0; x < loadImage.Width; x++)
+                for (int y = 0; y < loadImage.Height; y++)
                 {
                     pixel = loadImage.GetPixel(x, y);
                     processedImage.SetPixel(x, y, pixel);
@@ -48,7 +47,7 @@ namespace ImageProcessing
                 for (int y = 0; y < loadImage.Height; y++)
                 {
                     pixel = loadImage.GetPixel(x, y);
-                    grey = (byte) ((pixel.R + pixel.G + pixel.B)/3);
+                    grey = (byte)((pixel.R + pixel.G + pixel.B) / 3);
                     processedImage.SetPixel(x, y, Color.FromArgb(grey, grey, grey));
                 }
 
@@ -99,9 +98,9 @@ namespace ImageProcessing
                 }
 
             for (int x = 0; x < 256; x++)
-                for (int y = 0; y < Math.Min(hisdata[x]/5,800); y++)
+                for (int y = 0; y < Math.Min(hisdata[x] / 5, 800); y++)
                 {
-                    mydata.SetPixel(x, 799-y, Color.Black);
+                    mydata.SetPixel(x, 799 - y, Color.Black);
                 }
 
             pictureBox2.Image = mydata;
@@ -134,19 +133,292 @@ namespace ImageProcessing
             saveFileDialog1.Filter = "JPEG files (*.jpg)|*.jpg|PNG files (*.png)|*.png|All files (*.*)|*.*";
             saveFileDialog1.FilterIndex = 1;
             saveFileDialog1.ShowDialog();
-            
         }
 
         private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
-            
             processedImage.Save(saveFileDialog1.FileName);
-            
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            loadImage2 = new Bitmap(openFileDialog2.FileName);
+            pictureBox2.Image = loadImage2;
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+
+            Subtract(ref loadImage, ref loadImage2, ref processedImage);
+            pictureBox3.Image = processedImage;
+
+            btnLoadImageBack.Enabled = false;
+            btnLoadImageFront.Enabled = false;
+            btnSubtract.Enabled = false;
+            pictureBox3.Enabled = false;
+            label3.Enabled = false;
+            label2.Text = "Original";
+            label1.Text = "Processed";
+        }
+
+        private void btnLoadImageFront_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+        private void btnLoadImageBack_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void subtractToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnLoadImageBack.Enabled = true;
+            btnLoadImageFront.Enabled = true;
+            btnSubtract.Enabled = true;
+            pictureBox3.Enabled = true;
+            label3.Enabled = true;
+            label2.Text = "Front";
+            label1.Text = "Background";
+        }
+        private Device selectedDevice;
+        private void openToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Device[] devices = DeviceManager.GetAllDevices();
+            if (devices.Length > 0)
+            {
+                selectedDevice = DeviceManager.GetDevice(0);
+                selectedDevice.ShowWindow(pictureBox1);
+            }
+            else
+            {
+                MessageBox.Show("No webcam device found.");
+            }
+        }
+
+        /*private void brightnessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+                brightness(trackBar1.Value);
+        }
+
+        private void brightness(int val) {
+            processedImage = new Bitmap(loadImage.Width, loadImage.Height);
+            Color pixel;
+
+            for (int x = 0; x < loadImage.Width; x++)
+                for (int y = 0; y < loadImage.Height; y++)
+                {
+                    pixel = loadImage.GetPixel(x, y);
+                    if (val >= 0)
+                        processedImage.SetPixel(x, y, Color.FromArgb(Math.Min(pixel.R+val, 255), Math.Min(pixel.G + val, 255), Math.Min(pixel.B + val, 255)));
+                    else
+                        processedImage.SetPixel(x, y, Color.FromArgb(Math.Min(pixel.R - val, 0), Math.Min(pixel.G - val, 0), Math.Min(pixel.B - val, 0)));
+
+                }
+
+            pictureBox2.Image = processedImage;
+
+        }*/
+
 
         private void openToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             openFileDialog1.ShowDialog();
         }
+
+
+        public static void Subtract(ref Bitmap a, ref Bitmap b, ref Bitmap result)
+        {
+            if (b != null)
+            {
+                result = new Bitmap(b.Width, b.Height);
+
+                Color myGreen = Color.FromArgb(0, 0, 255);
+                int greygreen = (myGreen.R + myGreen.G + myGreen.B) / 3;
+                int threshold = 5;
+                for (int x = 0; x < b.Width; x++)
+                {
+                    for (int y = 0; y < b.Height; y++)
+                    {
+                        Color back = a.GetPixel(x, y);
+                        Color front = b.GetPixel(x, y);
+                        int grey = (front.R + front.G + front.B) / 3;
+                        int subtractVal = Math.Abs(greygreen - grey);
+                        if (subtractVal < threshold)
+                        {
+                            result.SetPixel(x, y, back);
+                        }
+                        else
+                        {
+                            result.SetPixel(x, y, front);
+                        }
+                    }
+
+                }
+            }
+        }
     }
+    public class Device
+    {
+        private const short WM_CAP = 0x400;
+        private const int WM_CAP_DRIVER_CONNECT = 0x40a;
+        private const int WM_CAP_DRIVER_DISCONNECT = 0x40b;
+        private const int WM_CAP_EDIT_COPY = WM_CAP + 30;
+        private const int WM_CAP_SET_PREVIEW = 0x432;
+        private const int WM_CAP_SET_OVERLAY = 0x433;
+        private const int WM_CAP_SET_PREVIEWRATE = 0x434;
+        private const int WM_CAP_SET_SCALE = 0x435;
+        private const int WS_CHILD = 0x40000000;
+        private const int WS_VISIBLE = 0x10000000;
+        private const int WM_CAP_SEQUENCE = WM_CAP + 62;
+        private const int WM_CAP_FILE_SAVEAS = WM_CAP + 23;
+        private const int SWP_NOMOVE = 0x20;
+        private const int SWP_NOSIZE = 1;
+        private const int SWP_NOZORDER = 0x40;
+        private const int HWND_BOTTOM = 1;
+
+
+        //	[DllImport("inpout32.dll", EntryPoint="Out32")]
+        //	public static extern void Output(int adress, int value);
+
+        //	[DllImport("inpout32.dll", EntryPoint="Inp32")]
+        //	public static extern int Input(int adress);
+
+        [DllImport("avicap32.dll")]
+        protected static extern int capCreateCaptureWindowA([MarshalAs(UnmanagedType.VBByRefStr)] ref string lpszWindowName,
+            int dwStyle, int x, int y, int nWidth, int nHeight, int hWndParent, int nID);
+
+        [DllImport("user32", EntryPoint = "SendMessageA")]
+        protected static extern int SendMessage(int hwnd, int wMsg, int wParam, [MarshalAs(UnmanagedType.AsAny)] object lParam);
+
+        [DllImport("user32")]
+        protected static extern int SetWindowPos(int hwnd, int hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
+
+        [DllImport("user32")]
+        protected static extern bool DestroyWindow(int hwnd);
+
+        int index;
+        int deviceHandle;
+
+        public Device()
+        {
+            //just a simple constructor
+        }
+        public Device(int index)
+        {
+            this.index = index;
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _version;
+
+        public string Version
+        {
+            get { return _version; }
+            set { _version = value; }
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+        /// <summary>
+        /// To Initialize the device
+        /// </summary>
+        /// <param name="windowHeight">Height of the Window</param>
+        /// <param name="windowWidth">Width of the Window</param>
+        /// <param name="handle">The Control Handle to attach the device</param>
+        public void Init(int windowHeight, int windowWidth, int handle)
+        {
+            string deviceIndex = Convert.ToString(this.index);
+            deviceHandle = capCreateCaptureWindowA(ref deviceIndex, WS_VISIBLE | WS_CHILD, 0, 0, windowWidth, windowHeight, handle, 0);
+
+            if (SendMessage(deviceHandle, WM_CAP_DRIVER_CONNECT, this.index, 0) > 0)
+            {
+                SendMessage(deviceHandle, WM_CAP_SET_SCALE, -1, 0);
+                SendMessage(deviceHandle, WM_CAP_SET_PREVIEWRATE, 0x42, 0);
+                SendMessage(deviceHandle, WM_CAP_SET_PREVIEW, -1, 0);
+
+                SetWindowPos(deviceHandle, 1, 0, 0, windowWidth, windowHeight, 6);
+            }
+        }
+
+        /// <summary>
+        /// Shows the webcam preview in the control
+        /// </summary>
+        /// <param name="windowsControl">Control to attach the webcam preview</param>
+        ///                    global::  
+        public void ShowWindow(System.Windows.Forms.Control windowsControl)
+        {
+            Init(windowsControl.Height, windowsControl.Width, windowsControl.Handle.ToInt32());
+        }
+
+        /// <summary>
+        /// Stop the webcam and destroy the handle
+        /// </summary>
+        public void Stop()
+        {
+            SendMessage(deviceHandle, WM_CAP_DRIVER_DISCONNECT, this.index, 0);
+            DestroyWindow(deviceHandle);
+        }
+        public void Sendmessage()
+        {
+            SendMessage(deviceHandle, WM_CAP_EDIT_COPY, 0, 0);
+        }
+
+    }
+    public class DeviceManager
+    {
+        //	[DllImport("inpout32.dll", EntryPoint="Out32")]
+        //	public static extern void Output(int adress, int value);
+
+        //	[DllImport("inpout32.dll", EntryPoint="Inp32")]
+        //	public static extern int Input(int adress);
+
+        [DllImport("avicap32.dll")]
+        protected static extern bool capGetDriverDescriptionA(short wDriverIndex,
+            [MarshalAs(UnmanagedType.VBByRefStr)] ref String lpszName,
+           int cbName, [MarshalAs(UnmanagedType.VBByRefStr)] ref String lpszVer, int cbVer);
+
+        static ArrayList devices = new ArrayList();
+
+        public static Device[] GetAllDevices()
+        {
+            String dName = "".PadRight(100);
+            String dVersion = "".PadRight(100);
+
+            for (short i = 0; i < 10; i++)
+            {
+                if (capGetDriverDescriptionA(i, ref dName, 100, ref dVersion, 100))
+                {
+                    Device d = new Device(i);
+                    d.Name = dName.Trim();
+                    d.Version = dVersion.Trim();
+
+                    devices.Add(d);
+                }
+            }
+
+            return (Device[])devices.ToArray(typeof(Device));
+        }
+
+        public static Device GetDevice(int deviceIndex)
+        {
+            return (Device)devices[deviceIndex];
+        }
+    }
+    
 }
